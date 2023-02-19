5c18c4b00ca45d2f462686f07842ff8d175e1f9d
heykittykitty <125777075+heykittykitty@users.noreply.github.com>
2023-02-19 01:45:11 -0600

    New ways

--- BabotsWifiAnalyzer.c
+++ BabotsWifiAnalyzer.c
@@ -1,95 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <net/if.h>
+#include <net/ethernet.h>
+#include <netinet/in.h> 
+#include <netinet/ip.h> 
+#include <netinet/tcp.h>
+#include <netinet/udp.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "network_scanner.h"
 
-// Function declarations
-void scan_networks();
-void retrieve_network_info(char *ssid, int *signal_strength, int *channel_usage);
-int scan_networks();
-void get_ssid(char* ssid);
-int get_signal_strength();
-int get_channel_usage();
+#define MAX_NETWORKS 10
 
-int main()
-{
-    char ssid[32];
-    int signal_strength;
-    int channel_usage;
-    
+int main(int argc, char *argv[]) {
     // Scan for available networks
-    printf("Scanning for available networks...\n");
-    scan_networks();
+    Network *networks[MAX_NETWORKS];
+    int num_networks = scan_networks(networks);
+    if (num_networks == 0) {
+        fprintf(stderr, "No networks found!\n");
+        return 0;
+    }
     
-    // Retrieve information about the networks
-    printf("Retrieving information about networks...\n");
-    retrieve_network_info(ssid, &signal_strength, &channel_usage);
+    // Connect to the first network
+    if (connect_to_network(networks[0]) == -1) {
+        fprintf(stderr, "Failed to connect to network!\n");
+        return 0;
+    }
     
-    // Print out the network information
-    printf("Network SSID: %s\n", ssid);
-    printf("Signal Strength: %d\n", signal_strength);
-    printf("Channel Usage: %d\n", channel_usage);
+    // Collect and analyze data
+    NetworkInfo *info = get_network_info(networks[0]);
+    NetworkParameters *params = get_network_parameters(networks[0]);
+    NetworkStats *stats = get_network_stats(networks[0]);
+    NetworkSecurity *security = get_network_security(networks[0]);
+    int signal_strength = get_network_signal_strength(networks[0]);
+    int num_devices = get_network_devices(networks[0]);
+    
+    // Print the collected data
+    printf("Network information:\n");
+    printf("\tName: %s\n", info->name);
+    printf("\tSignal Strength: %d\n", signal_strength);
+    printf("\tChannel: %d\n", params->channel);
+    printf("\tBandwidth: %d\n", params->bandwidth);
+    printf("\tData Rate: %d\n", params->data_rate);
+    printf("\tBytes Sent: %d\n", stats->bytes_sent);
+    printf("\tBytes Received: %d\n", stats->bytes_received);
+    printf("\tSecurity Protocol: %s\n", security->protocol);
+    printf("\tEncryption Type: %s\n", security->encryption_type);
+    printf("\tNumber of Devices Connected: %d\n", num_devices);
+    
+    // Disconnect from the network and clean up
+    disconnect_from_network(networks[0]);
+    reset_network_state(networks[0]);
+    int i;
+    for (i = 0; i < num_networks; i++) {
+        free(networks[i]);
+    }
     
     return 0;
-}
-
-// Scans for available networks
-void scan_networks()
-{
-    int num_networks = 0;
-    char ssid[32];
-    int signal_strength;
-    int channel_usage;
-    int i;
-    
-    printf("Scanning for networks...\n");
-    
-    // Scan for available networks
-    num_networks = scan_networks();
-    
-    // Print out the available networks
-    printf("Found %d networks:\n", num_networks);
-    for (i = 0; i < num_networks; i++) {
-        retrieve_network_info(ssid, &signal_strength, &channel_usage);
-        printf("  - SSID: %s, Signal Strength: %d, Channel Usage: %d\n", ssid, signal_strength, channel_usage);
-    }
-}
-
-// Retrieves information about a network
-void retrieve_network_info(char *ssid, int *signal_strength, int *channel_usage)
-{
-    // Get the SSID of the network
-    get_ssid(ssid);
-    
-    // Get the signal strength of the network
-    *signal_strength = get_signal_strength();
-    
-    // Get the channel usage of the network
-    *channel_usage = get_channel_usage();
-}
-
-// Scans for available networks and returns the number of networks found
-int scan_networks()
-{
-    // TODO: Write code to scan for available networks
-    return 0; // placeholder
-}
-
-// Gets the SSID of a network
-void get_ssid(char* ssid)
-{
-    // TODO: Write code to get the SSID of a network
-}
-
-// Gets the signal strength of a network
-int get_signal_strength()
-{
-    // TODO: Write code to get the signal strength of a network
-    return 0; // placeholder
-}
-
-// Gets the channel usage of a network
-int get_channel_usage()
-{
-    // TODO: Write code to get the channel usage of a network
-    return 0; // placeholder
 }
